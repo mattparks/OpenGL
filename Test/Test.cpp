577d289c35c28d380e6a942d1915de3e087136f2
Matthew Albrecht <mattparks5855@gmail.com>
2019-11-26 23:45:43 -0600

    Writing all the right stuff.

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 
+#include <GL/OpenGL.h>
+
 int main() {
+	std::cout << glInit() << "\n";
+	std::cout << (strstr(reinterpret_cast<const char *>(glGetString(GL_EXTENSIONS)), "GL_EXT_framebuffer_object") != nullptr);
+	auto ptr = glGetString(GL_EXTENSIONS);
 	std::cout << "Hello World!\n";
+	return 0;
 }
